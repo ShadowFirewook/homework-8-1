9aabde2e646567c0207b2ae0b9c5c555c43e0b75
ShadowFirewook <104684219+ShadowFirewook@users.noreply.github.com>
2022-05-22 13:13:52 +0300

    Update Magic.java

--- src/kg/geektech/game/players/Magic.java
+++ src/kg/geektech/game/players/Magic.java
@@ -9,7 +9,8 @@ public class Magic extends Hero {
     public void applySuperPower(Boss boss, Hero[] heroes) {
         for (int i = 0; i < heroes.length; i++) {
             heroes[i].setDamage(heroes[i].getDamage()+1);
-            System.out.println("Ìàã óâåëè÷èë óðîí ãåðîÿ " + heroes[i].getName() + " íà 1 åäèíèöó");
+            System.out.println("ÃŒÃ Ã£ Ã³Ã¢Ã¥Ã«Ã¨Ã·Ã¨Ã« Ã³Ã°Ã®Ã­ Ã£Ã¥Ã°Ã®Ã¿ " + heroes[i].getName() + " Ã­Ã  1 Ã¥Ã¤Ã¨Ã­Ã¨Ã¶Ã³");
         }
     }
 }
+ 
